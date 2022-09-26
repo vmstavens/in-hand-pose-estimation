b0ff46b0c2096617f0f2d5469c0e43adcf1ebd6d
Build Tools <build.tools@example.com>
2022-09-26 20:08:19 +0000

    on track for hand communication

--- sr_tactile_image/src/sr_tactile_image.cpp
+++ sr_tactile_image/src/sr_tactile_image.cpp
@@ -1,40 +1,72 @@
-#include "ros/node_handle.h"
+#include <cstddef>
 #include <ros/ros.h>
 #include <iostream>
-#include <sr_hand/hand_commander.hpp>
-// #include <sr_robot_msgs.h>
-#include <sr_robot_msgs/joint.h>
+#include "control_msgs/JointControllerState.h"
+#include "ros/duration.h"
+#include "ros/init.h"
+#include "ros/node_handle.h"
+#include "ros/service.h"
+#include "ros/subscriber.h"
+#include "sensor_msgs/JointState.h"
+#include <sr_hand/sr_subscriber.h>
+#include "sr_hand/hand/sr_articulated_robot.h"
+#include "sr_hand/hand/virtual_shadowhand.h"
+#include "sr_hand/hand_commander.hpp"
+#include "sr_robot_msgs/joints_data.h"
+#include <sr_utilities/sr_hand_finder.hpp>
+
+void callback_command()
+{
+    ROS_WARN_STREAM("I'm in a callback....");
+}
+
+
 int main (int argc, char **argv)
 {
+    
+	// std::vector<sr_robot_msgs::joint> home;
 
-	std::vector<sr_robot_msgs::joint> home;
-
+    const std::string NODE_NAME = "sr_tactile_image";
 
     // init node
-    ros::init(argc, argv, "sr_test");
+    ros::init(argc, argv, NODE_NAME);
 
     ros::NodeHandle nh;
 
-    // load hand controller 
-    auto hc = shadowrobot::HandCommander();
-    auto a = hc.get_all_joints();
-	// hc.sendCommands(std::vector<sr_robot_msgs::joint> joint_vector)
+    /*
+        • SRsubscriber/SRpublisher should not be used manually as they act from within the hand
+        • HandCommander needs a string, but i don't know which one? I have tried rostopic list -v [nodes] but no result
+        • SrHandFinder with no argument does not link, the ones shown below are tested
+            shadow_robot::SrHandFinder hf; 
+            shadow_robot::SrHandFinder hf = shadow_robot::SrHandFinder();
+        •
+        # Plan
+        • Run the simulation
+        • echo services and look for X +"/controller_manager/list_controllers" and locate X
+        • ros::service::waitForService("/sh_rh_wrj2_position_controller/set_gains",WAIT_TIME); // improve by waiting for service to be available or topic?
+    */
 
-	// hc.get_controller_state_topic(std::string joint_name)
+    const double WAIT_TIME = 3.0; // load from ros param server instead and put in config file
 
-    std::vector<sr_robot_msgs::joint> joints;
-    
-	for (auto& j : joints)
-        j.joint_target = 0.0;
+    ros::Rate rate(1.0/WAIT_TIME); // ROS Rate Hz
+    rate.sleep();
 
-	hc.sendCommands(joints);
+    shadowrobot::HandCommander hc = shadowrobot::HandCommander("/");
+    auto joint_names = hc.get_all_joints();
+    ROS_ERROR_STREAM("THE NUMBER OF JOINT NAMES FOUND : " << joint_names.size() << " ################################################");
+    auto joint_of_interest = joint_names[0];
+    // 1 | get the angle
+    ros::Subscriber sub = nh.subscribe<control_msgs::JointControllerState>("/sh_rh_ffj0_position_controller/state",1,callback_command);
+    // ros::Subscriber sub = nh.subscribe<sensor_msgs::JointState>(joint_of_interest,1,callback_command);
+    // └──╼ rostopic pub /sh_rh_ffj0_position_controller/command std_msgs/Float64 "data: 1.0" 
+    // /sh_rh_ffj0_position_controller/state [control_msgs/JointControllerState] 1 publisher
 
-    ros::Rate rate(1); // ROS Rate at 5Hz
-    
+    // ros::Subscriber sub = nh.subscribe<>(joint_of_interest,1,callback_command);
+
     while ( ros::ok() )
 	{
-        ROS_ERROR(a[0].c_str());
-        ROS_ERROR("Hello");
+        ROS_ERROR_STREAM(joint_names[0]);
+        ROS_WARN_STREAM("Hello");
         rate.sleep();
     }
 }
